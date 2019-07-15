14926d9455191703b4170e5fb912e9cfad176890
arakshit <arakshit@zaloni.com>
2019-07-15 19:43:31 +0530

    Spark Streaming example with nc

--- demo/src/main/java/com/streaming/demo/DemoApplication.java
+++ demo/src/main/java/com/streaming/demo/DemoApplication.java
@@ -1,13 +1,45 @@
 package com.streaming.demo;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        // Create a DStream that will connect to hostname:port, like localhost:9999
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+
+        jssc.start();              // Start the computation
+        jssc.awaitTermination();   // Wait for the computation to terminate
+    }
 }
