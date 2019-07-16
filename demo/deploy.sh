7324ce56d6d22db57862ee5d28571c2ab5a11c42
arakshit <arakshit@zaloni.com>
2019-07-16 20:19:48 +0530

    Added deploy script with nohup and stop script

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -jar target/demo-0.0.1-SNAPSHOT.jar -classpath ~/.m2/repository
+nohup java -jar target/demo-0.0.1-SNAPSHOT.jar -classpath ~/.m2/repository > demo-app.log &
+pId=$!
+echo "kill $pId" > ~/stop_demo-app.sh
+chmod u+x ~/stop_demo-app.sh
